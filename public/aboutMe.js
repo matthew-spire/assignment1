4e77c5c57f7bb1cf320fe7987681c91039bc0022
matthew-spire <matthew.thomas.spire@gmail.com>
2022-01-29 08:04:00 -0800

    first commit

---
+++ public/aboutMe.js
@@ -0,0 +1,8 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  className: "about"
+}, /*#__PURE__*/React.createElement("h1", null, "Matthew Spire"), /*#__PURE__*/React.createElement("img", {
+  src: "/images/MatthewSpire.jpg",
+  className: "me",
+  alt: "Picture of Matthew Spire"
+}));
+ReactDOM.render(element, document.getElementById('root'));
---
+++ src/aboutMe.jsx
@@ -0,0 +1,9 @@
+const element = (
+  <div className="about">
+    <h1>Matthew Spire</h1>
+
+    <img src="/images/MatthewSpire.jpg" className="me" alt="Picture of Matthew Spire"></img>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
